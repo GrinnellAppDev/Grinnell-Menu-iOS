3aae3e75cc034e865a4475ce95e71d7ad5ed32a9
deweytyl <ty.dewey@gmail.com>
2015-11-19 09:50:10 -0600

    Update favorites summary

--- G-licious/Model/FavoritesSummary.swift
+++ G-licious/Model/FavoritesSummary.swift
@@ -12,89 +12,81 @@ public class FavoritesSummary: NSObject {
 
     let serverURL = "http://appdev.grinnell.edu/glicious/"
     
-    public func favoriteDishesForDate(date: NSDate) -> [Dish]? {
-        var error: NSError?
-        var favDishes = [Dish]()
+    public func favoriteDishesForDate(date: NSDate, completionHandler: ([Dish]?, NSError?) -> Void) {
         
         // Get the day's dishes
-        let dishesArr: [Dish]?
-        do {
-            dishesArr = try self.retrieveDishesForDate(date)
-        } catch let error1 as NSError {
-            error = error1
-            dishesArr = nil
-        }
-        
-        if let err = error {                    // Dish retrieval was unsuccessful
-            NSLog("Error: \(err), \(err.userInfo)")
-            return nil
-        }
-        
-        let favoritesManager = FavoritesManager.sharedManager()
-        
-        if let dishes = dishesArr {
-            for dish in dishes {
-                if favoritesManager.containsFavorite(dish) {
+        retrieveDishesForDate(date) { (dishes, error) -> Void in
+            if let dishes = dishes {
+                let favoritesManager = FavoritesManager.sharedManager()
+                var favDishes = [Dish]()
+                
+                for dish in dishes where favoritesManager.containsFavorite(dish){
                     favDishes.append(dish)
                 }
+                
+                completionHandler(favDishes, nil)
+            } else if let err = error {                        // Dish retrieval was unsuccessful
+                NSLog("Error: \(err), \(err.userInfo)")
+                
+                completionHandler(nil, err)
+                
+            } else {
+                let err = NSError(domain: "GliciousServerErrorDomain", code: 3, userInfo: [NSLocalizedDescriptionKey: "Unknown Error"])
+                
+                completionHandler(nil, err)
             }
         }
-        
-        return favDishes
     }
     
-    private func retrieveDishesForDate(date: NSDate) throws -> [Dish] {
-        var error: NSError! = NSError(domain: "Migrator", code: 0, userInfo: nil)
-        
-        var jsonError: NSError?
-        var dishes = [Dish]()
+    private func retrieveDishesForDate(date: NSDate, completionHandler: ([Dish]?, NSError?) -> Void) {
         
         // Retrieve the year, month, and day components from the selected day
         let calendar = NSCalendar.currentCalendar()
         let components = calendar.components([.Year, .Month, .Day], fromDate: date)
         
-        //let dishesURL = NSURL(string: serverURL + "\(components.month)-\(components.day)-\(components.year)_dishes.json")!
+        let dishesURL = NSURL(string: serverURL + "\(components.month)-\(components.day)-\(components.year)_dishes.json")!
         
-        // test value
-        let dishesURL = NSURL(string: serverURL + "\(components.month)-\(13)-\(components.year)_dishes.json")!
         
-        let data = NSData(contentsOfURL:dishesURL)
-        
-        if (data == nil) {
-            let userInfo = [NSLocalizedDescriptionKey: "Failed to download dishes data"]
-            let err = NSError(domain: "GliciousServerErrorDomain", code: 1, userInfo: userInfo)
-            if (true) { error = err }
-            throw error
-        }
-        
-        // Parse dishes array object from JSON data
-        let dishesJSON: AnyObject?
-        do {
-            dishesJSON = try NSJSONSerialization.JSONObjectWithData(data!, options:[] )
-        } catch let error as NSError {
-            jsonError = error
-            dishesJSON = nil
-        }
-        
-        if let err = jsonError {                                // if parse failed
-            if (true) { error = err }
-            throw error
-        }
-        
-        // Cast JSON object into array of dish dictionaries
-        if let dishDicts = dishesJSON as? [[String: AnyObject]] {   // If cast succeeded
-            for dishDict in dishDicts {                             // Turn dish dicts into dishes
-                dishes.append(Dish(dishDictionary:dishDict))
+        let dataTask = NSURLSession.sharedSession().dataTaskWithURL(dishesURL) { (data, response, error) -> Void in
+            if let _ = error { // there was an error downloading the dishes
+                let userInfo = [NSLocalizedDescriptionKey: "Failed to download dishes data"]
+                let err = NSError(domain: "GliciousServerErrorDomain", code: 1, userInfo: userInfo)
+                
+                completionHandler(nil, err)
+                return
             }
-        } else {
-            let userInfo = [NSLocalizedDescriptionKey: "Failed to properly cast JSON object to dishes array"]
-            let err = NSError(domain: "GliciousServerErrorDomain", code: 2, userInfo: userInfo)
-            if (true) { error = err }
-            throw error
+            
+            // Parse dishes array object from JSON data
+            let dishesJSON: AnyObject?
+            do {
+                dishesJSON = try NSJSONSerialization.JSONObjectWithData(data!, options:[] )
+            } catch _ as NSError {
+                let userInfo = [NSLocalizedDescriptionKey: "Failed to properly cast JSON object to dishes array"]
+                let err = NSError(domain: "GliciousServerErrorDomain", code: 2, userInfo: userInfo)
+                
+                completionHandler(nil, err)
+                return
+            }
+            
+            var dishes = [Dish]()
+            
+            // Cast JSON object into array of dish dictionaries
+            if let dishDicts = dishesJSON as? [[String: AnyObject]] {   // If cast succeeds
+                for dishDict in dishDicts {                             // Turn dish dicts into dishes
+                    dishes.append(Dish(dishDictionary:dishDict))
+                }
+            } else {
+                let userInfo = [NSLocalizedDescriptionKey: "Failed to properly cast JSON object to dishes array"]
+                let err = NSError(domain: "GliciousServerErrorDomain", code: 2, userInfo: userInfo)
+                
+                completionHandler(nil, err)
+                return
+            }
+            
+            NSLog("\(dishes)")
+            completionHandler(dishes, nil)
         }
         
-        NSLog("\(dishes)")
-        
-        return dishes
-    } // retrieveDishesForDate(date:)
+        dataTask.resume()
+    } // retrieveDishesForDate(date:completionHandler:)
 }
